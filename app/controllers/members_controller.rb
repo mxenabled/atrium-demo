e6fdb1a72ab39cc583ca610f844cbe95e4921f65
dannie.brockbank <dannie.brockbank@mx.com>
2019-09-19 11:39:41 -0600

    create member through atrium api

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,18 +2,22 @@ class MembersController < ApplicationController
     def new 
         @code = params[:code]
         response = get_institution_creds(@code)
-        @credentials = response.credentials
+        @institution_credentials = response.credentials
+        p @credentials
+    end 
+
+    def create
+        member_credentials = params[:credentials].permit!().values
+        response = create_atrium_member(params[:institution_code], member_credentials, params[:user_guid])
+        p response
     end 
 
     private 
-
     def get_institution_creds(code)
-
         api_key = Rails.application.credentials.dig(:mx_api_key)
         client_id = Rails.application.credentials.dig(:mx_client_id)
         client = Atrium::AtriumClient.new("#{api_key}", "#{client_id}")
         institution_code = code
-        
         begin
           #Read institution credentials
           response = client.institutions.read_institution_credentials(institution_code)
@@ -23,4 +27,21 @@ class MembersController < ApplicationController
         end
     end
 
+    def create_atrium_member(code, credential_hash, user_guid)
+        member_info = {:member => {:institution_code => code, :credentials => credential_hash, :skip_aggregation => true}}
+        #member_info = {:member => member_parameters}
+        p member_info
+        api_key = Rails.application.credentials.dig(:mx_api_key)
+        client_id = Rails.application.credentials.dig(:mx_client_id)
+        client = Atrium::AtriumClient.new("#{api_key}", "#{client_id}")
+        body = Atrium::MemberCreateRequestBody.new(member_info) # MemberCreateRequestBody | Member object to be created with optional parameters (identifier and metadata) and required parameters (credentials and institution_code)
+        p body
+        begin
+          #Create member
+          response = client.members.create_member(user_guid, body)
+          p response
+        rescue Atrium::ApiError => e
+          puts "Exception when calling MembersApi->create_member: #{e}"
+        end
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   resources :members
   devise_for :users 
   as :user do
-    get 'users', :to => 'institutions#list', :as => :user_root # Rails 3
+    get 'users', :to => 'users#atrium_create', :as => :user_root # Rails 3
   end
-  root   'static_pages#home' 
+  root  'static_pages#home' 
   get   '/home',             to: 'static_pages#home'
   get   '/help',             to: 'static_pages#help'
   get   '/user_profile',     to: 'users#profile'
   get   '/institutions',     to: 'institutions#list'
-  get   '/user_stuff',       to: 'users#user_stuff'
-  get   '/add_member',       to: 'member#new'
-  
+  get   '/user_stuff',       to: 'users#atrium_create'
+  get   '/add_member',       to: 'members#new'
+  post  '/atrium_member',    to: 'members#create'    
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
