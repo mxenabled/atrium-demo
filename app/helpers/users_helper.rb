c05f8d3f6d981a9b520ef701a985863b0d81c7e2
dannie.brockbank <dannie.brockbank@mx.com>
2019-10-21 16:05:13 -0600

    removed some conditionals from the view and put it into a helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,19 @@
 module UsersHelper
+  def member_message(connection_status)
+    unless connection_status == "CONNECTED"
+      if connection_status == "RESUMED" || connection_status == "CREATED"
+        "Please wait while we finish importing all of your membership data!"
+      else 
+        "Whoops! It looks like there were some issues when trying to import your membership!"
+      end 
+    end 
+  end
+  
+  def member_has_issues(connection_status)
+    if connection_status == "CONNECTED" || connection_status == "RESUMED" || connection_status == "CREATED" 
+      false 
+    else
+      true 
+    end 
+  end 
 end
