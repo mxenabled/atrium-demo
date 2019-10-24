9544ee4fdb80b6131d28fb0ada68f8d3d2604581
dannie.brockbank <dannie.brockbank@mx.com>
2019-10-24 10:42:20 -0600

    fixing merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   as :user do
     get 'user', :to => 'users#show', :as => :user_root
   end
-<<<<<<< HEAD
 
   resources :members, :only => [:new, :create, :edit, :show, :update] do
     resources :registrations, :only => [:new, :create, :show], controller: 'members/registrations'
@@ -12,16 +11,8 @@ Rails.application.routes.draw do
 
   resources :accounts, :only => [:show]
   resources :institutions, :only => [:index]
-  resources :users, :only => [:show]
+  resources :git users, :only => [:show]
   
-=======
-  resources :accounts, :only => [:show]
-  resources :institutions, :only => [:index]
-  resources :members, :only => [:new, :create, :show]
-  resources :users, :only => [:show]
-
-
->>>>>>> 127d636b937b02bf7cd1e8f16de7ec2ea99b6a8b
   root  'static_pages#home' 
   get  '/home',                  to: 'static_pages#home'
   get  '/registrations/new',     to: 'members/registrations#new'
