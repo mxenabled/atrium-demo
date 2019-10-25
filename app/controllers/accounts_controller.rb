b7f1454beb14439f7b55d40207761a0667d79d65
dannie.brockbank <dannie.brockbank@mx.com>
2019-10-25 16:35:56 -0600

    changed from account_params to account_guid

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,24 +1,25 @@
 class AccountsController < ApplicationController
   def show
-    @account = read_account
+    p @account = read_account
     @transactions = list_transactions
   end
 
 private 
  
-  def account_params
-    params.permit(:account_guid)
+  def account_guid
+    account_params = params.permit(:account_guid)
+    account_params[:account_guid]
   end
 
   def list_transactions
-    transaction_response = client.accounts.list_account_transactions(account_params[:account_guid], current_user.guid)
+    transaction_response = client.accounts.list_account_transactions(account_guid, current_user.guid)
     transaction_response&.transactions
   rescue Atrium::ApiError => e
     Rails.logger.info "Exception when calling AccountsApi->list_account_transactions: #{e}"
   end 
 
   def read_account 
-    account_response = client.accounts.read_account(account_params[:account_guid], current_user.guid)
+    account_response = client.accounts.read_account(account_guid, current_user.guid)
     account_response&.account
   rescue Atrium::ApiError => e
     Rails.logger.info "Exception when calling AccountsApi->read_account: #{e}"
