9022ef76249743febe5e69219848f1b1ed5d7f9e
dannie.brockbank <dannie.brockbank@mx.com>
2019-11-04 11:01:40 -0700

    fixed a broken spec

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -27,9 +27,16 @@ RSpec.describe MembersController, type: :controller do
 
     context "when logged in" do 
 
-    it "sucessfully loads page" do
-      get :edit, :params => {:id => @member.id}
-      expect(response.status).to eq(200)
+      before(:each) do 
+        @user = FactoryBot.create(:user, guid: "USR-79941373-a635-4bc3-ac52-4cddbd74d6f9")
+        @member = FactoryBot.create(:member, guid: "MBR-85bbc828-3a5c-49fd-981b-0a9942617d83", user_id: @user.id)
+        sign_in @user
+      end 
+      
+      it "sucessfully loads page" do
+        get :edit, :params => {:id => @member.id}
+        expect(response.status).to eq(200)
+      end
     end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe UsersController, type: :controller do
       @user = FactoryBot.create(:user, guid: "USR-79941373-a635-4bc3-ac52-4cddbd74d6f9")
       sign_in @user
       FactoryBot.create(:member, guid: "MBR-85bbc828-3a5c-49fd-981b-0a9942617d83", user_id: @user.id)
-      get :show
+      get :show, :params => {:id => @user.id}
       expect(response.status).to eq(200)
     end 
   end 
