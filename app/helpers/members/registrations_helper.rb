62cd233679cf2a48fd40a8990400daf9be483b6d
dannie.brockbank <dannie.brockbank@mx.com>
2019-11-05 10:18:40 -0700

    update to accomodate adding mfa options

--- app/helpers/members/registrations_helper.rb
+++ app/helpers/members/registrations_helper.rb
@@ -1,12 +1,13 @@
 module Members::RegistrationsHelper
-  def mfa_challenge_type(challenge)
-    case challenge.type
-    when "IMAGE_DATA"
-     image_tag "#{challenge.image_data}"
-    when "OPTIONS"
-      #load options partial
-    else 
-      return
+  def mfa_image(challenge)
+    if challenge.type == "IMAGE_DATA"
+      image_tag "#{challenge.image_data}"
+    end
+  end
+
+  def mfa_type(challenge, f)
+    if challenge.type == "TEXT" || challenge.type == "IMAGE_DATA"
+      render partial: "input_mfa", locals: { f: f, challenge: challenge }
     end 
   end 
 end
