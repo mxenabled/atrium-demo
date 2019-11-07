15c033836599c89bcf743db04ddf28b9c3ea1992
dannie.brockbank <dannie.brockbank@mx.com>
2019-11-07 09:25:56 -0700

    last minute update

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resource :accounts, :only => [:show]
   resources :institutions, :only => [:index]
+  resources :users, :only => [:show]
   
   root  'static_pages#home' 
   get  '/home',                  to: 'static_pages#home'
