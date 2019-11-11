4c0313f96b229f1d4241d908449bffd5a2e458df
dannie.brockbank <dannie.brockbank@mx.com>
2019-11-11 14:06:56 -0700

    spacing issue

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -57,8 +57,6 @@ protected
       Rails.logger.info "Exception when calling UsersApi->delete_user: #{e}"
     end
 
-
-
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
